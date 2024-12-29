73b007064f5572569ed454a9a6ee117e200ce113
Jeffrey Stevens <51374986+JeffreyRStevens@users.noreply.github.com>
2024-12-29 11:58:33 -0600

    Only include hometown and high school for year 2024

--- R/player_stats.R
+++ R/player_stats.R
@@ -6,15 +6,18 @@
 #' `find_team_id()`.
 #'
 #' @returns
-#' Tibble of player statistics.
+#' Tibble of player statistics. Note that hometown and high school were added
+#' in 2024.
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' player_stats("585290")
+#' player_stats(find_team_id("Nebraska", 2024))
+#' player_stats(find_team_id("UCLA", 2023, sport = "MVB"))
 #' }
 player_stats <- function(team_id) {
-  check_team_id(team_id)
+  yr <- check_team_id(team_id)
   url <- paste0("https://stats.ncaa.org/teams/", team_id, "/season_to_date_stats")
 
   player_stats <- request_url(url) |>
@@ -24,17 +27,23 @@ player_stats <- function(team_id) {
     dplyr::rename("Number" = "#") |>
     dplyr::mutate(Number = as.numeric(.data$Number))
 
-  url2 <- paste0("https://stats.ncaa.org/teams/", team_id, "/roster")
-  roster <- request_url(url2) |>
-    httr2::resp_body_html() |>
-    rvest::html_element("table") |>
-    rvest::html_table() |>
-    dplyr::select("Number" = "#", "Name", "Hometown", "High School") |>
-    dplyr::mutate(Number = as.numeric(.data$Number))
+  if (yr == 2024) {
+    url2 <- paste0("https://stats.ncaa.org/teams/", team_id, "/roster")
+    roster <- request_url(url2) |>
+      httr2::resp_body_html() |>
+      rvest::html_element("table") |>
+      rvest::html_table() |>
+      dplyr::select("Number" = "#", "Name", "Hometown", "High School") |>
+      dplyr::mutate(Number = as.numeric(.data$Number))
 
-  dplyr::left_join(player_stats, roster,
-                   by = dplyr::join_by("Number", "Player" == "Name")) |>
-    dplyr::relocate("Hometown":"High School", .after = "Ht") |>
-    dplyr::mutate(dplyr::across("Kills":"PTS", ~ as.numeric(gsub(",", "", .x)))) |>
-    dplyr::arrange(.data$Number)
+    dplyr::left_join(player_stats, roster,
+                     by = dplyr::join_by("Number", "Player" == "Name")) |>
+      dplyr::relocate("Hometown":"High School", .after = "Ht") |>
+      dplyr::mutate(dplyr::across("Kills":"PTS", ~ as.numeric(gsub(",", "", .x)))) |>
+      dplyr::arrange(.data$Number)
+  } else {
+    player_stats |>
+      dplyr::mutate(dplyr::across("Kills":"PTS", ~ as.numeric(gsub(",", "", .x)))) |>
+      dplyr::arrange(.data$Number)
+  }
 }
