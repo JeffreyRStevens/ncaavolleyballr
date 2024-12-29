37bf59a0cc424a1f63e4e0b290335f969ba54e83
Jeffrey Stevens <51374986+JeffreyRStevens@users.noreply.github.com>
2024-12-29 11:57:06 -0600

    Make wvb_teams and mvb_teams internal data

---
+++ data-raw/teams.R
@@ -0,0 +1,24 @@
+## code to prepare `wvb_teams` datasets
+
+years <- 2020:2024
+
+wvb_teams <- purrr::map2(rep(years, times = 3),
+                        rep(1:3, each = length(years)),
+                        ~ get_teams(year = .x, division = .y)) |>
+  purrr::list_rbind() |>
+  dplyr::filter(team_id != "598395") # remove Vanderbilt 2024
+
+usethis::use_data(wvb_teams, overwrite = TRUE)
+
+## code to prepare `mvb_teams` datasets
+
+years <- 2020:2024
+
+mvb_teams <- purrr::map2(rep(years, times = 2),
+                         rep(c(1, 3), each = length(years)),
+                         ~ get_teams(year = .x, division = .y, sport = "MVB")) |>
+  purrr::list_rbind()
+
+usethis::use_data(mvb_teams, overwrite = TRUE)
+
+usethis::use_data(mvb_teams, wvb_teams, internal = TRUE, overwrite = TRUE)
