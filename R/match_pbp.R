6e2a80cb8ad34847e7c642f268f0ceae559b478e
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2025-01-02 17:59:20 -0600

    Create, document, and test match_pbp()

---
+++ R/match_pbp.R
@@ -0,0 +1,39 @@
+#' Extract play-by-play information for a particular match
+#'
+#' The NCAA's page for a match/contest includes a tab called
+#' "Play By Play". This function extracts the tables of play-by-play information
+#' for each set.
+#'
+#' @inheritParams player_match_stats
+#'
+#' @returns
+#' Returns a list with as many data frames as there are sets. Each data frame
+#' has a column for events happening for each team, as well as a column for the
+#' score.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' match_pbp(contest = "6080706")
+#' }
+match_pbp <- function(contest = NULL) {
+  if (is.null(contest)) cli::cli_abort(paste0("Enter valid contest ID as a character string."))
+  if (!is.character(contest)) cli::cli_abort("Enter valid contest ID as a character string.")
+
+  url <- paste0("https://stats.ncaa.org/contests/", contest, "/play_by_play")
+
+  pbp_all <- request_url(url) |>
+    httr2::resp_body_html() |>
+    rvest::html_elements("table") |>
+    rvest::html_table()
+  match_info <- pbp_all[[1]]
+
+  num_sets <- match_info[1, which(match_info[1, ] == "S") - 1] |>
+    dplyr::pull() |>
+    as.numeric()
+  sets <- 4:(3 + num_sets)
+
+  purrr::lmap(sets, ~`[[`(pbp_all, .x)) |>
+    purrr::set_names(nm = paste0("Set ", 1:num_sets))
+}
---
+++ tests/testthat/test-match_pbp.R
@@ -0,0 +1,14 @@
+suppressWarnings(final2024 <- match_pbp(contest = "6080706"))
+
+test_that("match_pbp() works", {
+  expect_equal(length(final2024),
+               4)
+  expect_equal(length(final2024[[1]]$Louisville),
+               455)
+  expect_equal(final2024[[1]]$Louisville[1],
+               "Match started")
+  expect_error(match_pbp(),
+               "Enter valid contest ID as a character string")
+  expect_error(match_pbp(contest = 585290),
+               "Enter valid contest ID as a character string")
+})
