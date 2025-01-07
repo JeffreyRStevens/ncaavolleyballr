62960e1ba307a50ba481b6f44ca9066c9c8133dc
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2025-01-07 09:19:34 -0600

    Update data

--- data-raw/ncaa.R
+++ data-raw/ncaa.R
@@ -6,7 +6,8 @@ wvb_teams <- purrr::map2(rep(years, times = 3),
                         rep(1:3, each = length(years)),
                         ~ get_teams(year = .x, division = .y)) |>
   purrr::list_rbind() |>
-  dplyr::filter(team_id != "598395" & team_id != "585523" & team_id != "504440") # remove Vanderbilt 2024, Saint Augustine's 2024, Georgetown 2020
+  dplyr::mutate(team_name = stringr::str_trim(team_name)) #|>
+  # dplyr::filter(team_id != "598395" & team_id != "585523" & team_id != "504440") # remove Vanderbilt 2024, Saint Augustine's 2024, Georgetown 2020
 
 usethis::use_data(wvb_teams, overwrite = TRUE)
 
@@ -14,12 +15,11 @@ usethis::use_data(wvb_teams, overwrite = TRUE)
 
 ## code to prepare `mvb_teams` dataset
 
-years <- 2020:2024
-
 mvb_teams <- purrr::map2(rep(years, times = 2),
                          rep(c(1, 3), each = length(years)),
                          ~ get_teams(year = .x, division = .y, sport = "MVB")) |>
-  purrr::list_rbind()
+  purrr::list_rbind() |>
+  dplyr::mutate(team_name = stringr::str_trim(team_name))
 
 usethis::use_data(mvb_teams, overwrite = TRUE)
 
