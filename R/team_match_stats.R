a5037d8bb7ae92ff7ec0e5ce14510c3602be3b4e
Jeffrey Stevens <51374986+JeffreyRStevens@users.noreply.github.com>
2025-01-12 14:54:40 -0600

    Create, document, and test team_match_stats()

---
+++ R/team_match_stats.R
@@ -0,0 +1,72 @@
+
+#' Extract team summary statistics for all matches in a particular season
+#'
+#' The NCAA's main page for a team includes a tab called "Game By Game"
+#' and a section called "Game by Game Stats".
+#' This function extracts the team's summary statistics for each match of the
+#' season.
+#'
+#' @inheritParams player_season_stats
+#' @inheritParams team_season_stats
+#'
+#' @returns
+#' Returns a data frame of summary team statistics for each match of the season.
+#'
+#' @export
+#'
+#' @family functions that extract team statistics
+#'
+#' @examples
+#' \dontrun{
+#' team_match_stats(team_id = "585290")
+#' team_match_stats(team_id = find_team_id("Nebraska", 2024), opponent = TRUE)
+#' team_match_stats(team_id = "573670", sport = "MVB")
+#' }
+team_match_stats <- function(team_id = NULL,
+                             opponent = FALSE,
+                             sport = "WVB") {
+  # check input
+  check_team_id(team_id)
+  check_logical("opponent", opponent)
+  team_df <- check_sport(sport, vb_only = TRUE)
+
+  # get team info and request URL
+  team_info <- get_team_info(team_id) #|>
+  team_url <- paste0("https://stats.ncaa.org/teams/", team_id)
+  resp <- request_url(team_url)
+
+  # extract arena info
+  gbg_page <- resp |> httr2::resp_body_html() |>
+    rvest::html_elements(".nav-link") |>
+    rvest::html_attr("href") |>
+    stringr::str_subset("/players/\\d+")
+  gbg_url <- paste0("https://stats.ncaa.org/", gbg_page)
+
+  tables <- request_url(gbg_url) |>
+    httr2::resp_body_html() |>
+    rvest::html_elements("table") |>
+    rvest::html_table()
+  # team <- tables[[1]]$Team[1]
+
+  matches <- tables[[2]] |>
+    dplyr::mutate(Date = dplyr::na_if(.data$Date, ""),
+                  team_opp = ifelse(.data$Opponent == "Defensive Totals", "Opponent", "Team"),
+                  Opponent = dplyr::na_if(.data$Opponent, "Defensive Totals"),
+                  Result = dplyr::na_if(.data$Result, ""),
+                  .after = "Opponent") |>
+    dplyr::mutate(Team = team_info$team_name[1],
+                  Conference = team_info$conference[1],
+                  .after = "Date") |>
+    tidyr::fill("Date", "Opponent", "Result") |>
+    dplyr::mutate(dplyr::across("S":dplyr::last_col(), ~ suppressWarnings(as.numeric(gsub(",", "", .x)))))
+
+  if(!opponent) {
+    matches |>
+      dplyr::filter(.data$team_opp == "Team") |>
+      dplyr::select(!"team_opp")
+  } else {
+    matches |>
+      dplyr::filter(.data$team_opp == "Opponent") |>
+      dplyr::select(!"team_opp")
+  }
+}
