6461ad97d4769bc468745dc82dcd50120c8fb028
Jeffrey Stevens <51374986+JeffreyRStevens@users.noreply.github.com>
2025-01-19 11:52:56 -0600

    Include all html_table_raw() subfunctions to eliminate :::

--- R/utils.R
+++ R/utils.R
@@ -180,11 +180,129 @@ html_table_raw <- function(x,
                            dec = ".",
                            na.strings = "NA",
                            convert = TRUE) {
+  compact <- function(.x) {
+    Filter(length, .x)
+  }
+  dw_find <- function(dw, col) {
+    match <- col == dw$col
+    list(
+      col = dw$col[match],
+      rowspan = dw$rowspan[match],
+      colspan = dw$colspan[match],
+      text = dw$text[match]
+    )
+  }
+  dw_init <- function() {
+    list(
+      col = integer(),
+      rowspan = integer(),
+      colspan = integer(),
+      text = character()
+    )
+  }
+
+  dw_add <- function(dw, col, rowspan, colspan, text) {
+    dw$col <-     c(dw$col, col)
+    dw$text <-    c(dw$text, text)
+    dw$rowspan <- c(dw$rowspan, rowspan)
+    dw$colspan <- c(dw$colspan, colspan)
+    dw
+  }
+
+  dw_prune <- function(dw) {
+    dw$rowspan <- dw$rowspan - 1L
+    keep <- dw$rowspan > 0L
+
+    dw$col <-     dw$col[keep]
+    dw$text <-    dw$text[keep]
+    dw$rowspan <- dw$rowspan[keep]
+    dw$colspan <- dw$colspan[keep]
+    dw
+  }
+  table_fill <- function(cells, trim = TRUE) {
+    width <- 0
+    height <- length(cells) # initial estimate
+    values <- vector("list", height)
+
+    # list of downward spanning cells
+    dw <- dw_init()
+
+    # https://html.spec.whatwg.org/multipage/tables.html#algorithm-for-processing-rows
+    for (i in seq_along(cells)) {
+      row <- cells[[i]]
+      if (length(row) == 0) {
+        next
+      }
+
+      rowspan <- as.integer(rvest::html_attr(row, "rowspan", default = NA_character_))
+      rowspan[is.na(rowspan)] <- 1
+      colspan <- as.integer(rvest::html_attr(row, "colspan", default = NA_character_))
+      colspan[is.na(colspan)] <- 1
+      text <- row
+      if (isTRUE(trim)) {
+        text <- gsub("^[[:space:]\u00a0]+|[[:space:]\u00a0]+$", "", text)
+      }
+
+      vals <- rep(NA_character_, width)
+      col <- 1
+      j <- 1
+      while(j <= length(row)) {
+        if (col %in% dw$col) {
+          cell <- dw_find(dw, col)
+          cell_text <- cell$text
+          cell_colspan <- cell$colspan
+        } else {
+          cell_text <- text[[j]]
+          cell_colspan <- colspan[[j]]
+
+          if (rowspan[[j]] > 1) {
+            dw <- dw_add(dw, col, rowspan[[j]], colspan[[j]], text[[j]])
+          }
+
+          j <- j + 1
+        }
+        vals[col:(col + cell_colspan - 1L)] <- cell_text
+        col <- col + cell_colspan
+      }
+
+      # Add any downward cells after last <td>
+      for(j in seq(col - 1L, width)) {
+        if (j %in% dw$col) {
+          cell <- dw_find(dw, j)
+          vals[j:(j + cell$colspan - 1L)] <- cell$text
+        }
+      }
+
+      dw <- dw_prune(dw)
+      values[[i]] <- vals
+
+      height <- max(height, i + max(rowspan) - 1L)
+      width <- max(width, col - 1L)
+    }
+
+    # Add any downward cells after <tr>
+    i <- length(values) + 1
+    length(values) <- height
+    while (length(dw$col) > 0) {
+      vals <- rep(NA_character_, width)
+      for (col in dw$col) {
+        cell <- dw_find(dw, col)
+        vals[col:(col + cell$colspan - 1L)] <- cell$text
+      }
+      values[[i]] <- vals
+      i <- i + 1
+      dw <- dw_prune(dw)
+    }
+
+    values <- lapply(values, `[`, seq_len(width))
+    matrix(unlist(values), ncol = width, byrow = TRUE)
+  }
+
 
   ns <- xml2::xml_ns(x)
   rows <- xml2::xml_find_all(x, ".//tr", ns = ns)
   cells <- lapply(rows, xml2::xml_find_all, ".//td|.//th", ns = ns)
-  cells <- rvest:::compact(cells)
+  cells <- compact(cells)
 
   if (length(cells) == 0) {
     return(tibble::tibble())
@@ -269,81 +387,3 @@ save_df <- function(x, label, group, year, division, conf, sport, path) {
 #'
 #' @keywords internal
 #'
-table_fill <- function(cells, trim = TRUE) {
-  width <- 0
-  height <- length(cells) # initial estimate
-  values <- vector("list", height)
-
-  # list of downward spanning cells
-  dw <- rvest:::dw_init()
-
-  # https://html.spec.whatwg.org/multipage/tables.html#algorithm-for-processing-rows
-  for (i in seq_along(cells)) {
-    row <- cells[[i]]
-    if (length(row) == 0) {
-      next
-    }
-
-    rowspan <- as.integer(rvest::html_attr(row, "rowspan", default = NA_character_))
-    rowspan[is.na(rowspan)] <- 1
-    colspan <- as.integer(rvest::html_attr(row, "colspan", default = NA_character_))
-    colspan[is.na(colspan)] <- 1
-    text <- row
-    if (isTRUE(trim)) {
-      text <- gsub("^[[:space:]\u00a0]+|[[:space:]\u00a0]+$", "", text)
-    }
-
-    vals <- rep(NA_character_, width)
-    col <- 1
-    j <- 1
-    while(j <= length(row)) {
-      if (col %in% dw$col) {
-        cell <- rvest:::dw_find(dw, col)
-        cell_text <- cell$text
-        cell_colspan <- cell$colspan
-      } else {
-        cell_text <- text[[j]]
-        cell_colspan <- colspan[[j]]
-
-        if (rowspan[[j]] > 1) {
-          dw <- rvest:::dw_add(dw, col, rowspan[[j]], colspan[[j]], text[[j]])
-        }
-
-        j <- j + 1
-      }
-      vals[col:(col + cell_colspan - 1L)] <- cell_text
-      col <- col + cell_colspan
-    }
-
-    # Add any downward cells after last <td>
-    for(j in seq(col - 1L, width)) {
-      if (j %in% dw$col) {
-        cell <- rvest:::dw_find(dw, j)
-        vals[j:(j + cell$colspan - 1L)] <- cell$text
-      }
-    }
-
-    dw <- rvest:::dw_prune(dw)
-    values[[i]] <- vals
-
-    height <- max(height, i + max(rowspan) - 1L)
-    width <- max(width, col - 1L)
-  }
-
-  # Add any downward cells after <tr>
-  i <- length(values) + 1
-  length(values) <- height
-  while (length(dw$col) > 0) {
-    vals <- rep(NA_character_, width)
-    for (col in dw$col) {
-      cell <- rvest:::dw_find(dw, col)
-      vals[col:(col + cell$colspan - 1L)] <- cell$text
-    }
-    values[[i]] <- vals
-    i <- i + 1
-    dw <- rvest:::dw_prune(dw)
-  }
-
-  values <- lapply(values, `[`, seq_len(width))
-  matrix(unlist(values), ncol = width, byrow = TRUE)
-}
