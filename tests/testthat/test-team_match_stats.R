a4113a973f57e8ca08029237a5594fa46f44c468
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2025-07-22 10:43:40 -0500

    Test for dataframe size returned by team_match_stats()

--- tests/testthat/test-team_match_stats.R
+++ tests/testthat/test-team_match_stats.R
@@ -3,6 +3,8 @@ test_that("team_match_stats() works", {
   skip_on_ci()
   chromote::local_chrome_version("system", quiet = TRUE)
   expect_silent(neb2024 <- team_match_stats(team_id = "585290"))
+  expect_equal(nrow(neb2024), 36)
+  expect_equal(ncol(neb2024), 21)
   expect_equal(neb2024[neb2024$Date == "08/27/2024", ]$Kills, 47)
 })
 
