b3369a744c8d5246b0246edd6ca26333c4d7cfca
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2025-07-22 16:50:24 -0500

    Add season to team_match_stats() output

--- tests/testthat/test-team_match_stats.R
+++ tests/testthat/test-team_match_stats.R
@@ -4,7 +4,7 @@ test_that("team_match_stats() works", {
   chromote::local_chrome_version("system", quiet = TRUE)
   expect_silent(neb2024 <- team_match_stats(team_id = "585290"))
   expect_equal(nrow(neb2024), 36)
-  expect_equal(ncol(neb2024), 21)
+  expect_equal(ncol(neb2024), 22)
   expect_equal(neb2024[neb2024$Date == "08/27/2024", ]$Kills, 47)
 })
 
